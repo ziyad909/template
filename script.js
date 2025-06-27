ff58390507dd7628dac1c418cc5ceef97260e5bc
ziyad909 <ziyadch11062006@gmail.com>
2025-06-27 01:38:16 +0100

    the dark theme mode

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 
+const themeSwitcher = document.getElementById("theme-switcher");
+// function that make the text content and the icon of dark mode
+function darkMode(){
+    themeSwitcher.children[0].textContent = "Dark Mode";
+    themeSwitcher.children[1].classList.replace("fa-sun","fa-moon");
+}
+// function that make the text content and icon of light mode
+function lightMode(){
+     themeSwitcher.children[0].textContent="Light Mode";
+     themeSwitcher.children[1].classList.replace("fa-moon","fa-sun");
+}
+
+// function that switch the theme mode
+
+function switchTheme(){
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+
+    if(!currentTheme || currentTheme === 'dark'){
+        document.documentElement.setAttribute( "data-theme","light");
+        localStorage.setItem("theme","light");
+        lightMode();
+    }
+    else{
+        document.documentElement.setAttribute( "data-theme","dark");
+        localStorage.setItem("theme","dark");
+        darkMode();
+    }
+}
+
+
+// add an Event listener on click on the themeSwitcher
+
+themeSwitcher.addEventListener("click",switchTheme);
+
+// get the theme from the local storage 
+
+const themeFromLocaleStorage = localStorage.getItem("theme");
+
+if(themeFromLocaleStorage){
+    document.documentElement.setAttribute("data-theme",themeFromLocaleStorage);
+    if(themeFromLocaleStorage === 'dark'){
+        darkMode();
+    }else{
+        lightMode();
+    }
+}
