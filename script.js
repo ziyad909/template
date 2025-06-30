f4d7341adfd1f6f5d74a93ebac01c45c8a3064c7
ziyad909 <ziyadch11062006@gmail.com>
2025-06-30 03:08:05 +0100

    typographix project with navigation menu

--- script.js
+++ script.js
@@ -45,3 +45,19 @@ if(themeFromLocaleStorage){
         lightMode();
     }
 }
+
+// the navigation menu 
+
+const nav = document.getElementById("nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+
+function toggleMenu(){
+    nav.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+}
+
+function hideMenu(){
+    nav.classList.remove("active");
+    menuIcon.classList.remove("active");
+}
